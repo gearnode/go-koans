635af8107c3ef32fac35e787444d0fcc2023ce84
Gearnode <bfrimin@student.42.fr>
2017-04-23 16:32:55 +0200

    Add discover pointer land

--- koans_test.go
+++ koans_test.go
@@ -24,3 +24,85 @@ func BenchmarkHello(b *testing.B) {
   }
 }
 
+// Pointer
+
+// Notes
+// Go have pointer but go has no pointer arithmetic :(
+
+func TestPointer(t *testing.T) {
+  // Use {} to isolate context into go func
+  // Relly usefull to have many idependant
+  // assertation into func Test
+  {
+    x := 3
+    y := x // b is a copy of a
+    // All assignments in golang are copy operation
+
+    y++
+
+    if x == y {
+      t.FailNow()
+    }
+  }
+  {
+    x := 3
+    y := &x // use & to access to adress of a pointer
+
+    if *y != 3 {
+      t.FailNow()
+    }
+
+    // y is a pointer of x
+    // Use * symbol to de-referencing
+    *y = *y + 2
+
+    if x != 5 {
+      t.FailNow()
+    }
+  }
+  {
+    incr := func(i int) int {
+      i = i + 1
+      return i
+    }
+
+    x := 1
+    incr(x)
+
+    if x != 1 {
+      t.FailNow()
+    }
+  }
+  {
+    incr := func(i *int) *int {
+      *i++
+      return i
+    }
+
+    x := 1
+    incr(&x)
+    if x != 2 {
+      t.FailNow()
+    }
+  }
+  {
+    x := 1
+    p := &x
+    pp := &p
+    cp := **pp
+
+    if **pp != x {
+      t.FailNow()
+    }
+
+    **pp = 4
+    if x != 4 {
+      t.FailNow()
+    }
+
+    if cp != 1 {
+      t.FailNow()
+    }
+  }
+
+}
